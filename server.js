7055ad0c6ca836471ada9aab573bcb9035f2624a
Ferid HELALI <92591821+helaliferid@users.noreply.github.com>
2021-12-06 12:39:45 +0100

    scaffold the Todo Api

--- server.js
+++ server.js
@@ -5,38 +5,52 @@ const lib= require('./lib');
 const hostname = '127.0.0.1';
 const port = 8080;
 
-const todos=[
-    {id:1,label:'email parcing',isDone:false},
-    {id:2,label:'Write todos for the day',isDone:false},
-    {id:3,label:'Review yesterday code',isDone:false},
-    {id:4,label:'Implement new feature',isDone:false},
-]
 
-//console.log(new lib.TodoList())
+//  {id:1,label:'email parcing',isDone:false},
+
 
 const server = http.createServer(function(req,res){
     //handle request data
-    let _url=parser.urlWithStringQuery(req.url);
+    let _url=parser.parse(req.url,true);
+    let method=req.method;
+
+    setHeadersToRequest(res,200,{type:'Content-Type',value:'application/json'});
+
+    switch (method) {
+        case 'POST':
+            if(_url.pathname==="/addTodo"){
+                //TODO: retrieve the body of the request and passe it to SaveInfFile
+                res.end(`{"message":"addTodo Works"}`)
+            }else{
+                res.end(`{"message":"This is a TODO LIST API"}`);
+            }
+            
+            break;
+        case 'GET':
+            if(_url.pathname==="/getTodos"){
+                 //TODO: retrieve todos from the file
+                res.end(`{"message":"getTodos Works"}`)
+            }else{
+                res.end(`{"message":"This is a TODO LIST API"}`);
+            }  
+            break;
     
-    // processing 
-    let toDoList=new lib.TodoList();
-    toDoList.addElementToList(  {id:1,label:'email parcing'});
-    toDoList. addElementToList( {id:2,label:'Write todos for the day'});
-    res.statusCode=301;
-    res.setHeader('Content-Type','text/html');
-    res.setHeader('token','mean-stack');
-    if(res.statusCode===404)
-        res.end("{'Error' :'( Notfound'}")
-    else{ 
-        res.end(`
-            {
-            list:${JSON.stringify(toDoList._list,undefined,3)},
-            _rul:${JSON.stringify(parser.parse(_url,true),undefined,3)}
-        }`);
+        default:
+            res.end(`{"message":"This is a TODO LIST API"}`);
+            break;
     }
+
+
+    
   
 });
 
 server.listen(port,hostname,function(){
     console.log(`Server listening on ${hostname}:${port}`);
 })
+
+
+function setHeadersToRequest(res,_statusCode,header){
+    res.statusCode=_statusCode;
+    res.setHeader(header.type,header.value);
+}
